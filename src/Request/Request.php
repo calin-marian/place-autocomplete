a41e016c8f611e36b677c1bfdf8a1cfe72232a08
Calin Marian <calin.marian@dreamproduction.com>
2016-11-22 18:04:16 +0100

    Fixed Request comments, namespaces, and typehinting

--- src/Request/Request.php
+++ src/Request/Request.php
@@ -7,7 +7,9 @@ namespace GooglePlaceAutocomplete\Request;
  * Contains GooglePlaceAutocomplete\Request\Request.
  */
 
+use GooglePlaceAutocomplete\Component\ComponentInterface;
 use GooglePlaceAutocomplete\Exception\RequestException;
+use GooglePlaceAutocomplete\Parameter\Location;
 use GooglePlaceAutocomplete\Parameter\LocationInterface;
 use GooglePlaceAutocomplete\Parameter\PlaceType;
 use Fig\Cache\Memory\MemoryPool;
@@ -52,14 +54,7 @@ class Request implements RequestInterface {
   private $cachePool;
 
   /**
-   * The result of the query.
-   *
-   * @var array
-   */
-  private $result;
-
-  /**
-   * Constructor for Query.
+   * Constructor for Request.
    *
    * @param string $key
    *   The Google API key.
@@ -105,7 +100,7 @@ class Request implements RequestInterface {
         $error_msg = strtr("Request failed with status: @status.", array('@status' => $response->getStatusCode()));
         throw new RequestException($error_msg, 1);
       }
-      
+
       $responseBody = $response->getBody()->getContents();
 
       // Save these to cache for future requests.
@@ -183,10 +178,7 @@ class Request implements RequestInterface {
   }
 
   /**
-   * Set the types parameter for the request.
-   *
-   * @param \GooglePlaceAutocomplete\Parameter\PlaceType $placeType
-   * @return $this
+   * {@inheritdoc}
    */
   public function setTypes(PlaceType $placeType) {
     $this->options['types'] = $placeType;
@@ -195,21 +187,7 @@ class Request implements RequestInterface {
   }
 
   /**
-   * Set the offest parameter for the request.
-   *
-   * The position, in the input term, of the last character that the service
-   * uses to match predictions. For example, if the input is 'Google' and the
-   * offset is 3, the service will match on 'Goo'. The string determined by the
-   * offset is matched against the first word in the input term only. For
-   * example, if the input term is 'Google abc' and the offset is 3, the service
-   * will attempt to match against 'Goo abc'. If no offset is supplied, the
-   * service will use the whole term. The offset should generally be set to the
-   * position of the text caret.
-   *
-   * @param int $offset
-   * @return $this
-   *
-   * @throws \InvalidArgumentException
+   * {@inheritdoc}
    */
   public function setOffset($offset) {
     // Validate the offset is integer.
@@ -223,30 +201,16 @@ class Request implements RequestInterface {
   }
 
   /**
-   * Set the location parameter for the request.
-   *
-   * The point around which you wish to retrieve place information.
-   *
-   * @param \GooglePlaceAutocomplete\Parameter\LocationInterface $location
-   * @return $this
+   * {@inheritdoc}
    */
-  public function setLocation(LocationInterface $location) {
+  public function setLocation(Location $location) {
     $this->options['location'] = $location;
 
     return $this;
   }
 
   /**
-   * Set the radius parameter for the request.
-   *
-   * The distance (in meters) within which to return place results. Note that
-   * setting a radius biases results to the indicated area, but may not fully
-   * restrict results to the specified area. See Location Biasing below.
-   *
-   * @param int $radius
-   * @return $this
-   *
-   * @throws \InvalidArgumentException
+   * {@inheritdoc}
    */
   public function setRadius($radius) {
     // Validate the radius is integer.
@@ -260,16 +224,7 @@ class Request implements RequestInterface {
   }
 
   /**
-   * Set the language paramenter for the request.
-   *
-   * The language in which the results should be returned, if possible. Searches
-   * are also biased to the selected language; results in the selected language
-   * may be given a higher ranking. If language is not supplied, the Place
-   * Autocomplete service will attempt to use the native language of the domain
-   * from which the request is sent.
-   *
-   * @param \GoogleSupportedLanguages\Languages\LanguageInterface $language
-   * @return $this
+   * {@inheritdoc}
    */
   public function setLanguage(LanguageInterface $language) {
     $this->options['language'] = $language;
@@ -278,16 +233,9 @@ class Request implements RequestInterface {
   }
 
   /**
-   * Set the components paramenter for the request.
-   *
-   * A grouping of places to which you would like to restrict your results.
-   * Currently, you can use components to filter by country. For example:
-   * components=country:fr would restrict your results to places within France
-   *
-   * @param \GooglePlaceAutocomplete\Request\ComponentsInterface $components
-   * @return $this
+   * {@inheritdoc}
    */
-  public function setComponents(ComponentsInterface $components) {
+  public function setComponents(ComponentInterface $components) {
     $this->options['components'] = $components;
 
     return $this;
--- src/Request/RequestInterface.php
+++ src/Request/RequestInterface.php
@@ -1,30 +1,109 @@
 <?php
 
-namespace Dreamproduction\GooglePlacesAPI\Request;
+namespace GooglePlaceAutocomplete\Request;
+
+use GooglePlaceAutocomplete\Component\ComponentInterface;
+use GooglePlaceAutocomplete\Parameter\Location;
+use GooglePlaceAutocomplete\Parameter\PlaceType;
+use GoogleSupportedLanguages\Languages\LanguageInterface;
 
 /**
- * Contains Dreamproduction\GooglePlacesAPI\Request\RequestInterface.
+ * Contains GooglePlaceAutocomplete\Request\RequestInterface.
  */
 
 interface RequestInterface {
 
   /**
-   * Performs the search on the Places API.
+   * Execute the request.
    *
-   * @param string $input
-   *   The query string.
-   * @param array $options
-   * @return QueryInterface
-   * @throws \Dreamproduction\GooglePlacesAPI\Exception\RequestException
-   */
-  public function query($input, $options = array());
-
-  /**
-   * Execute the query.
-   *
-   * @return array
-   *   The results.
+   * @return \GooglePlaceAutocomplete\Response\Response
+   * @throws \GooglePlaceAutocomplete\Exception\RequestException
    */
   public function execute();
+
+  /**
+   * Set the imput for the autocomplete.
+   *
+   * @param string $input
+   * @return $this
+   */
+  public function setInput($input);
+
+  /**
+   * Set the types parameter for the request.
+   *
+   * @param \GooglePlaceAutocomplete\Parameter\PlaceType $placeType
+   * @return $this
+   */
+  public function setTypes(PlaceType $placeType);
+
+  /**
+   * Set the offest parameter for the request.
+   *
+   * The position, in the input term, of the last character that the service
+   * uses to match predictions. For example, if the input is 'Google' and the
+   * offset is 3, the service will match on 'Goo'. The string determined by the
+   * offset is matched against the first word in the input term only. For
+   * example, if the input term is 'Google abc' and the offset is 3, the service
+   * will attempt to match against 'Goo abc'. If no offset is supplied, the
+   * service will use the whole term. The offset should generally be set to the
+   * position of the text caret.
+   *
+   * @param int $offset
+   * @return $this
+   *
+   * @throws \InvalidArgumentException
+   */
+  public function setOffset($offset);
+
+  /**
+   * Set the location parameter for the request.
+   *
+   * The point around which you wish to retrieve place information.
+   *
+   * @param \GooglePlaceAutocomplete\Parameter\Location $location
+   * @return $this
+   */
+  public function setLocation(Location $location);
+
+  /**
+   * Set the radius parameter for the request.
+   *
+   * The distance (in meters) within which to return place results. Note that
+   * setting a radius biases results to the indicated area, but may not fully
+   * restrict results to the specified area. See Location Biasing below.
+   *
+   * @param int $radius
+   * @return $this
+   *
+   * @throws \InvalidArgumentException
+   */
+  public function setRadius($radius);
+
+  /**
+   * Set the language paramenter for the request.
+   *
+   * The language in which the results should be returned, if possible. Searches
+   * are also biased to the selected language; results in the selected language
+   * may be given a higher ranking. If language is not supplied, the Place
+   * Autocomplete service will attempt to use the native language of the domain
+   * from which the request is sent.
+   *
+   * @param \GoogleSupportedLanguages\Languages\LanguageInterface $language
+   * @return $this
+   */
+  public function setLanguage(LanguageInterface $language);
+
+  /**
+   * Set the components paramenter for the request.
+   *
+   * A grouping of places to which you would like to restrict your results.
+   * Currently, you can use components to filter by country. For example:
+   * components=country:fr would restrict your results to places within France
+   *
+   * @param \GooglePlaceAutocomplete\Component\ComponentInterface $components
+   * @return $this
+   */
+  public function setComponents(ComponentInterface $components);
 
 }
